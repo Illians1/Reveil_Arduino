ade5a415d4fa9fd69357a3622af9331056c6368d
Illians1 <aouidamehdi@gmail.com>
2025-01-27 11:56:07 +0100

    27/01

--- bluetooth_classic_manager.cpp
+++ bluetooth_classic_manager.cpp
@@ -6,16 +6,14 @@ BluetoothA2DPSink a2dp_sink(i2s);
 // Constantes pour les boutons et délais
 const int BUTTON_INCREASE_PIN = 32;
 const int BUTTON_DECREASE_PIN = 33;
-const int BUTTON_NEXT_PIN = 16;
-const int BUTTON_PREV_PIN = 17;
 
 // Pins pour l'encodeur rotatif KY-040
-const int PIN_CLK = 25;    // Pin CLK de l'encodeur
-const int PIN_DT = 26;     // Pin DT de l'encodeur
-const int PIN_SW = 27;     // Pin SW (bouton) de l'encodeur
+const int PIN_CLK = 16;  // Pin CLK de l'encodeur
+const int PIN_DT = 17;   // Pin DT de l'encodeur
+const int PIN_SW = 13;   // Pin SW (bouton) de l'encodeur
 
 // Variables pour l'encodeur
-volatile int lastCLK;      // Dernier état de CLK
+volatile int lastCLK;           // Dernier état de CLK
 volatile int volumeLevel = 50;  // Volume initial (0-100)
 
 const unsigned long TIMEOUT_STEP = 300000;  // 5 minutes en millisecondes
@@ -41,13 +39,9 @@ void gestion_connexion(esp_a2d_connection_state_t state, void *ptr) {
 
 void init_bluetooth() {
   // Configuration des boutons
-  
+
   pinMode(BUTTON_INCREASE_PIN, INPUT_PULLUP);
   pinMode(BUTTON_DECREASE_PIN, INPUT_PULLUP);
-
-  // Configuration des nouveaux boutons
-  pinMode(BUTTON_NEXT_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_PREV_PIN, INPUT_PULLUP);
 
   // Définir la fonction de callback pour détecter la connexion
   a2dp_sink.set_on_connection_state_changed(gestion_connexion);
@@ -68,20 +62,19 @@ void init_bluetooth() {
     if (i < 5) Serial.print(":");
   }
   Serial.println();
-  
-/*
+
   // Configuration de l'encodeur rotatif
   pinMode(PIN_CLK, INPUT);
   pinMode(PIN_DT, INPUT);
   pinMode(PIN_SW, INPUT_PULLUP);
   
   lastCLK = digitalRead(PIN_CLK);  // Lire l'état initial
-  */
+
 }
 
 void handle_bluetooth() {
   // Gestion des boutons
- 
+
   if (digitalRead(BUTTON_INCREASE_PIN) == LOW) {
     if (!activateDisplay()) {
       delay(200);
@@ -126,23 +119,6 @@ void handle_bluetooth() {
     delay(200);
   }
 
-  // Gestion des boutons de contrôle de la musique
-  if (digitalRead(BUTTON_NEXT_PIN) == LOW) {
-
-    if (a2dp_sink.is_connected()) {
-      a2dp_sink.next();
-    }
-    delay(200);
-  }
-
-  if (digitalRead(BUTTON_PREV_PIN) == LOW) {
-
-    if (a2dp_sink.is_connected()) {
-      a2dp_sink.previous();
-    }
-    delay(200);
-  }
-
   // Vérifier si un appareil est connecté et si le délai est dépassé
   if (a2dp_sink.is_connected()) {
     unsigned long tempsEcoule = millis() - derniereMiseAJour;
@@ -152,7 +128,7 @@ void handle_bluetooth() {
       derniereMiseAJour = millis();
     }
   }
-/*
+
   // Gestion de l'encodeur rotatif
   if (a2dp_sink.is_connected()) {
     // Gestion de la rotation pour le volume
@@ -182,7 +158,7 @@ void handle_bluetooth() {
         delay(200);
         return;
       }
-      
+
       if (isPlaying) {
         a2dp_sink.pause();
         isPlaying = false;
@@ -195,7 +171,6 @@ void handle_bluetooth() {
       delay(200);  // Debounce
     }
   }
-  */
 }
 
 // Callback optionnel pour recevoir les métadonnées de la piste en cours
